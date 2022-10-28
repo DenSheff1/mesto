fb05730f3fbb3339de51ff36cf38a5fd9ed8a502
Denis Shevale <Denis-Practicum@yandex.ru>
2022-10-28 17:36:12 +0300

    First review bugfixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,31 @@
 let popup = document.querySelector('.popup');
-let formElement = document.querySelector('.popup__container');
+let formElement = document.querySelector('.popup__form-userinfo');
 let profileEditButton = document.querySelector('.profile__button-edit')
 let popupCloseButton = document.querySelector('.popup__button-close');
 let popupSubmitButton = document.querySelector('.popup__button-submit');
 let profileName = document.querySelector('.profile__title');
 let profileAbout = document.querySelector('.profile__subtitle');
-let nameInput = document.querySelector('#username');
-let jobInput = document.querySelector('#userjob');
+let nameInput = document.querySelector('.popup__input_userinfo_name');
+let jobInput = document.querySelector('.popup__input_userinfo_job');
 
 function showPopup() {
-  popup.classList.add('popup__opened');
+  popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileAbout.textContent;
 }
 
 function hidePopup() {
-  popup.classList.remove('popup__opened');
+  popup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileAbout.textContent = jobInput.value;
+    hidePopup();
 }
 
 profileEditButton.addEventListener('click', showPopup);
 popupCloseButton.addEventListener('click', hidePopup);
 formElement.addEventListener('submit', formSubmitHandler);
-popupSubmitButton.addEventListener('click', hidePopup);
+
