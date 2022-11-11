ec15edd1105fccfab8ddf9b69c1ddff2140b853f
Denis Shevale <Denis-Practicum@yandex.ru>
2022-11-11 20:44:21 +0300

    refactoring: Переделал появление карточек из Template

--- scripts/index.js
+++ scripts/index.js
@@ -1,99 +1,107 @@
+//данные для карточек
 const initialCards = [
   {
     name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-    alt: 'Архыз'
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
   },
   {
     name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-    alt: 'Челябинская область'
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
   },
   {
     name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-    alt: 'Иваново'
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
   },
   {
     name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-    alt: 'Камчатка'
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
   },
   {
     name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-    alt: 'Холмогорский район'
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
   },
   {
     name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-    alt: 'Байкал'
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
 
+// переменные, которые потребуются для генерации карточек
 const cardContainer = document.querySelector('.cards__list');
+const cardTemplate = document.querySelector('#card-template').content.querySelector('.card');
 
+//создаем карточки
+const generateCard = (everyCardData) => {
+  const instanceCard = cardTemplate.cloneNode(true);
+
+  const cardTitle = instanceCard.querySelector('.card__title');
+  cardTitle.textContent = everyCardData.name;
+
+  const cardPhoto = instanceCard.querySelector('.card__photo');
+  cardPhoto.src = everyCardData.link;
+  cardPhoto.alt = everyCardData.name;
+
+  return instanceCard;
+}
+
+//добавляем созданные карточки в контейнер на странице
 const renderCard = (everyCardData) => {
-  cardContainer.insertAdjacentHTML("afterbegin",
-  `<li class="card">
-  <article class="article">
-    <img src=${everyCardData.link} alt=${everyCardData.alt} class="card__photo">
-    <div class="card__footer">
-      <h3 class="card__title">${everyCardData.name}</h3>
-      <button class="card__button card__button_type_like" type="button"></button>
-    </div>
-  </article>
-</li>
-`
-);
+  cardContainer.prepend(generateCard(everyCardData));
 };
 
+//обходим массив с объектами, в каждом собираем инфу
 initialCards.forEach((everyCardData) => {
   renderCard(everyCardData);
 });
 
+//открытие и закрытие попапов
+const popupEdit = document.querySelector('.popup__edit-profile');
+const popupAdd = document.querySelector('.popup__add-card');
+const btnEdit = document.querySelector('.profile__button-edit');
+const btnAdd = document.querySelector('.profile__button-add');
+const btnCloseAddCard = document.querySelector('.popup__button-close-add-card');
+const btnCloseEditProf = document.querySelector('.popup__button-close-edit-profile');
 
+function openPopup(popupElem) {
+  popupElem.classList.add('popup_opened');
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-let popup = document.querySelector('.popup');
-let formElement = document.querySelector('.popup__form-userinfo');
-let profileEditButton = document.querySelector('.profile__button-edit')
-let popupCloseButton = document.querySelector('.popup__button-close');
-let popupSubmitButton = document.querySelector('.popup__button-submit');
-let profileName = document.querySelector('.profile__title');
-let profileAbout = document.querySelector('.profile__subtitle');
-let nameInput = document.querySelector('.popup__input_userinfo_name');
-let jobInput = document.querySelector('.popup__input_userinfo_job');
-
-function showPopup() {
-  popup.classList.add('popup_opened');
+btnEdit.addEventListener('click', function() {
+  openPopup(popupEdit);
   nameInput.value = profileName.textContent;
   jobInput.value = profileAbout.textContent;
-}
+});
 
-function hidePopup() {
-  popup.classList.remove('popup_opened');
-}
+btnAdd.addEventListener('click', function() {
+  openPopup(popupAdd);
+});
 
-function formSubmitHandler (evt) {
+function closePopup(popupElem) {
+  popupElem.classList.remove('popup_opened');
+};
+
+btnCloseAddCard.addEventListener('click', function() {
+  closePopup(popupAdd);
+});
+
+btnCloseEditProf.addEventListener('click', function() {
+  closePopup(popupEdit);
+});
+
+//работа с данными пользователя в профиле
+const formProfile = document.querySelector('.popup__form-userinfo');
+const profileName = document.querySelector('.profile__title');
+const profileAbout = document.querySelector('.profile__subtitle');
+const nameInput = document.querySelector('.popup__input_userinfo_name');
+const jobInput = document.querySelector('.popup__input_userinfo_job');
+
+function formProfileSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileAbout.textContent = jobInput.value;
-    hidePopup();
+    closePopup(popupEdit);
 }
 
-profileEditButton.addEventListener('click', showPopup);
-popupCloseButton.addEventListener('click', hidePopup);
-formElement.addEventListener('submit', formSubmitHandler);
+formProfile.addEventListener('submit', formProfileSubmitHandler);
+
+
