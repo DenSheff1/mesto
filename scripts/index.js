4fd7ec383b9823435287ec4d1d8731f0643b16c5
Denis Shevale <Denis-Practicum@yandex.ru>
2022-11-11 16:57:20 +0300

    feature: Добавил карточки с помощью AdjascentHTML

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,72 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+    alt: 'Архыз'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+    alt: 'Челябинская область'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+    alt: 'Иваново'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+    alt: 'Камчатка'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+    alt: 'Холмогорский район'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+    alt: 'Байкал'
+  }
+];
+
+const cardContainer = document.querySelector('.cards__list');
+
+const renderCard = (everyCardData) => {
+  cardContainer.insertAdjacentHTML("afterbegin",
+  `<li class="card">
+  <article class="article">
+    <img src=${everyCardData.link} alt=${everyCardData.alt} class="card__photo">
+    <div class="card__footer">
+      <h3 class="card__title">${everyCardData.name}</h3>
+      <button class="card__button card__button_type_like" type="button"></button>
+    </div>
+  </article>
+</li>
+`
+);
+};
+
+initialCards.forEach((everyCardData) => {
+  renderCard(everyCardData);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let popup = document.querySelector('.popup');
 let formElement = document.querySelector('.popup__form-userinfo');
 let profileEditButton = document.querySelector('.profile__button-edit')
@@ -28,4 +97,3 @@ function formSubmitHandler (evt) {
 profileEditButton.addEventListener('click', showPopup);
 popupCloseButton.addEventListener('click', hidePopup);
 formElement.addEventListener('submit', formSubmitHandler);
-
