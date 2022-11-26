8be43b7ea85ce50bc72f746a6632f3667ffac03c
Denis Shevale <Denis-Practicum@yandex.ru>
2022-11-26 20:01:24 +0300

    refactoring: изменение js

--- scripts/validate.js
+++ scripts/validate.js
@@ -5,13 +5,22 @@ function setSubmitButtonState(isFormValid) {
   if (isFormValid) {
     editSubmit.removeAttribute('disabled');
     editSubmit.classList.remove('popup__button-submit_disabled');
+    addSubmit.removeAttribute('disabled');
+    addSubmit.classList.remove('popup__button-submit_disabled');
   } else {
     editSubmit.setAttribute('disabled', true);
     editSubmit.classList.add('popup__button-submit_disabled');
+    addSubmit.setAttribute('disabled', true);
+    addSubmit.classList.add('popup__button-submit_disabled');
   }
 }
 
 formProfile.addEventListener('input', function (evt) {
-  const isValid = nameInput.value.length > 0 && jobInput.value.length > 0;
-  setSubmitButtonState(isValid);
+  const editIsValid = nameInput.value.length > 0 && jobInput.value.length > 0;
+  setSubmitButtonState(editIsValid);
+});
+
+formAddCard.addEventListener('input', function (evt) {
+  const addIsValid = placeName.value.length > 0 && placeLink.value.length > 0;
+  setSubmitButtonState(addIsValid);
 });
