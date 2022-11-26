bf60ea3d539f02664f70cacba04e4600f003a513
Denis Shevale <Denis-Practicum@yandex.ru>
2022-11-26 21:13:32 +0300

    Спринт 7/11 → Тема 5/9: Валидация форм → Урок 4/7

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,3 +1,62 @@
+const formElement = document.querySelector('.popup__form-userinfo');
+const formInput = formElement.querySelector('.popup__input_userinfo_name');
+const formError = formElement.querySelector(`.${formInput.id}-error`);
+
+const showInputError = (elem, errMessage) => {
+  elem.classList.add('popup__input_type_error');
+  formError.textContent = errMessage;
+  formError.classList.add('form_input-message-error_active');
+};
+
+const hideInputError = (elem) => {
+  elem.classList.remove('popup__input_type_error');
+  formError.classList.remove('form_input-message-error_active');
+  formError.textContent = '';
+};
+
+const isValid = () => {
+  if(!formInput.validity.valid) {
+    showInputError(formInput, formInput.validationMessage);
+  } else {
+    hideInputError(formInput);
+  }
+};
+
+formInput.addEventListener('input', isValid);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const addSubmit = document.querySelector('.popup__button-submit_type_add');
 const editSubmit = document.querySelector('.popup__button-submit_type_edit');
 
